4b9d1ff327d48d68b811c7fd846f373bdd92acfe
tiennmhe <taixexedo@gmail.com>
2023-09-25 14:34:53 +0700

    update

--- src/main/java/com/example/project/controller/LoginController.java
+++ src/main/java/com/example/project/controller/LoginController.java
@@ -1,18 +1,28 @@
 package com.example.project.controller;
 
 
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.project.entity.user;
 import com.example.project.service.UserService;
 
+import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.criteria.Path;
 import jakarta.servlet.http.HttpSession;
-
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 /**
  * LoginController
  */
@@ -20,7 +30,7 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/login")
 public class LoginController {
   private final UserService userService;
-
+  
   public LoginController(UserService userService) {
     this.userService = userService;
   }
@@ -50,4 +60,9 @@ public class LoginController {
     }
     return "login";
   }
+
+  
+
+
+
 }
