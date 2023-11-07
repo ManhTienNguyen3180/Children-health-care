48325ed2d947d5a3d3db6af67b757f7f799b0812
tiennmhe <taixexedo@gmail.com>
2023-11-08 00:56:51 +0700

    update iter 3

--- src/main/java/com/example/project/Repository/ServiceCategoryRepo.java
+++ src/main/java/com/example/project/Repository/ServiceCategoryRepo.java
@@ -1,12 +1,22 @@
 package com.example.project.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.project.entity.category_service;
+import com.example.project.entity.service;
 
 @Repository
 public interface ServiceCategoryRepo extends JpaRepository<category_service,Integer> {
-    
+    @Query("select c from category_service c where c.status = 1")
+    List<category_service> findAllCategoryService(); 
+     
+
+
+     
+
 }
 
